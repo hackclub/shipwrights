9d5a4d88fffb83c707ca7dc5934cb0198df957c3
Eobam <117958786+Eobam@users.noreply.github.com>
2026-02-23 08:09:00 -0500

    Fix typos and improve FAQ answers

--- sw-list/src/app/faq/qna.ts
+++ sw-list/src/app/faq/qna.ts
@@ -1,12 +1,12 @@
 export const FAQ = {
   General: [
     {
-      q: 'What is shipwrights?',
+      q: 'What is Shipwrights?',
       a: 'We review and certify ships from Hack Club orginized YSWS - Flavortown! \n Basically, we make sure your project is working and easy demoable before giving you that sweet approval. \n Think of us as your friendly project helpers :)',
     },
     {
       q: 'How long does it take?',
-      a: 'Depends on the queue, but usually 1-7 days! Sometimes faster if we are vibing. Check out the /queue page for current stats!',
+      a: 'Depends on the queue, but usually 1-7 days! Sometimes faster if we are vibing. Check out the <a href="https://us.review.hackclub.com/queue">queue</a> page for current stats!',
     },
     {
       q: 'What counts as a ship?',
@@ -14,8 +14,19 @@ export const FAQ = {
     },
     {
       q: 'How do I become a Shipwright?',
-      a: 'Shipwrights are invite-only! We look for people who are consistently active, positive, helpful, and genuinely interested in being part of the team. Just be yourself, be friendly, and if you are doing great work, we will notice you :)',
+      a: 'Shipwrights are invite-only! We look for people who are consistently active, positive, helpful, and genuinely interested in being part of the team. Just be yourself, be friendly, and if you are doing great work, we will notice you. You can see this amazing message from Nullskulls <a href="https://hackclub.slack.com/archives/C099P9FQQ91/p1765392376942069">here</a> regarding joining the Shipwrights for more info! :).',
     },
+    {
+      q: 'Whats the difference between the Shipwrights and the YSWS Team?',
+      a: 'The Shipwrights look at your demo, README, and repository. While the YSWS Team checks your commit and devlog activity, as well as double-checking to make sure we didnt make any mistakes in our review process (which we do!)'
+    },
+    {
+      q: 'How come some projects get reviewed faster than others?',
+      a: 'Although pending projects are in a "queue" we dont review projects by oldest in queue. The reason for this is that some Shipwrights can only review certain types of projects, meaning that we just review what we can test with our knowledge and devices.'
+    },
+    {
+      q: 'I have a question that is not answered here or is project specific!!',
+      a: 'You can ask in <a href="https://hackclub.enterprise.slack.com/archives/C099P9FQQ91">#ask-the-shipwrights</a> on the Slack! We actively look at this channel and are happy to discuss your project!'
   ],
   Certification: [
     {
@@ -24,19 +35,27 @@ export const FAQ = {
     },
     {
       q: 'I got rejected for a broken demo link?',
-      a: "There is many reason why your demo link could be rejected. Make sure it's working, and does not return any errors before resubmitting! \n Rejections by the System are semi-automated with real person supervision, but sometimes it can make mistakes:(",
+      a: "There are many reasons why your demo link could be rejected. Make sure it's working and does not return any errors before resubmitting! \n Rejections by the System are semi-automated with real person supervision, but sometimes it can make mistakes:(",
     },
     {
       q: 'I got rejected for a broken readme link?',
-      a: 'The most common rejection is a non-raw readme url. Make sure your add a raw readme link to your project settings, and that the url does not return 404 or any other errors before resubmitting!',
+      a: 'The most common rejection is a non-raw readme url. Make sure you add a raw readme link to your project settings, and that the url does not return 404 or any other errors before resubmitting!',
     },
     {
       q: 'I got rejected for a broken repo link?',
-      a: "Make sure your repo is public and the URL leads to your source code! It cannot be linked to any files, nor Readme's.",
+      a: "Make sure your repo is public, and the URL leads to your source code! It cannot be linked to any files, nor Readme's.",
     },
     {
       q: 'Can I change links after I shipped?',
-      a: 'No, once you ship, you cannot change the any of the project links. You will have to wait for it to be rejected (if links are broken/incorect) and resubmit your project.',
+      a: 'No, once you ship, you cannot change any of the project links. You will have to wait for it to be rejected (if links are broken/incorect) and resubmit your project.',
     },
+    {
+      q: 'Why do you require a raw README link?',
+      a: 'The Flavortown platform requires a raw README so that certain systems can load the README correctly!',
+    },
+    {
+      q: 'Where can I find all the demo rules?',
+      a: 'Most demo rules can be found in the <a href="https://hackclub.enterprise.slack.com/docs/T0266FRGM/F0A5WD2QKTP">Ships! Ships! Ships!</a> canvas on the Slack! We try to update this and include the most up-to-date info there!'
+
   ],
 }
